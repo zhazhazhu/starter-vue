41be5b8e61836200c7530126eab3724eb6ecbd14
zhazhazhu <fangyuan.leslie@qq.com>
2023-11-28 10:21:16 +0800

    chore: add eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,33 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "./.eslintrc-auto-import.json",
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  plugins: ["prettier", "vue", "@typescript-eslint"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "vue/valid-template-root": "off",
+    "@typescript-eslint/no-unnecessary-type-constraint": "off",
+  },
 }
