d1f6bacb69d019b04343d7c9d359b9e3f9c2f660
zhazhazhu <fangyuan.leslie@qq.com>
2023-11-28 10:26:44 +0800

    chore: add auto-pages and auto-import and @vueuse/core

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,30 +6,30 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "./.eslintrc-auto-import.json",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    './.eslintrc-auto-import.json'
   ],
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest'
   },
-  plugins: ["prettier", "vue", "@typescript-eslint"],
+  plugins: ['prettier', 'vue', '@typescript-eslint'],
   rules: {
-    "vue/multi-word-component-names": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "vue/valid-template-root": "off",
-    "@typescript-eslint/no-unnecessary-type-constraint": "off",
-  },
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'vue/valid-template-root': 'off',
+    '@typescript-eslint/no-unnecessary-type-constraint': 'off'
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Pages from 'vite-plugin-pages'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    Pages(),
+    AutoImport({
+      vueTemplate: true,
+      dirs: ['./src/composables'],
+      imports: ['vue', '@vueuse/core', 'pinia', 'vue-router'],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      }
+    })
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
